e99ae07d782bc45b1163ea0f9cd4507b4a942f51
Luke <lukeoxley9@gmail.com>
2024-01-30 19:48:06 -0500

    Passing current main base hardware tests

--- scripts/rules_constants.py
+++ scripts/rules_constants.py
@@ -9,3 +9,8 @@ R_IMD_TRIP_TEST_RESISTANCE_PERCENT = 0.5 # IN.4.4.2 50% of response value
 R_BSPD_MAX_TRIP_TIME_S = 0.5  # EV.7.7.2
 R_BSPD_POWER_THRESH_W  = 5000 # EV.7.7.2
 
+# Precharge and Discharge EV.5.6
+R_PCHG_V_BAT_THRESH = 0.90 # EV.5.6.1.a
+
+# TSAL EV.5.9
+R_TSAL_HV_V = 60.0 # T.9.1.1
