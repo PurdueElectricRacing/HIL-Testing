1aad878a0f8e55913dba530c63083b38583a624a
Millan Kumar <millankumar@gmail.com>
2024-11-09 20:11:44 -0500

    Trying to test "AI" mode (unsuccessful?)

--- scripts/test_millan.py
+++ scripts/test_millan.py
@@ -13,21 +13,35 @@ def test_do_di(hil: HIL):
 	hil_in = hil.din("Millan", "HIL_IN")
 
 	for _i in range(3):
-		print("\nHIL_OUT: 0")
-		hil_out.state = 0
-		hil_in_state = hil_in.state
-		same = hil_in_state == 0
-		print(f"HIL_IN: {hil_in_state} == 0: {same}")
+		for state in range(2): # 0, 1
+			print("\nHIL_OUT: 0")
+			hil_out.state = state
+			hil_in_state = hil_in.state
+			same = hil_in_state == state
+			print(f"HIL_IN: {state} == {hil_in_state}: {same}")
 
-		time.sleep(3)
+			time.sleep(1)
 
-		print("\nHIL_OUT: 1")
-		hil_out.state = 1
-		hil_in_state = hil_in.state
-		same = hil_in_state == 1
-		print(f"HIL_IN: {hil_in_state} == 1: {same}")
+		print()
+# ---------------------------------------------------------------------------- #
 
-		time.sleep(3)
+# ---------------------------------------------------------------------------- #
+def test_do_ai(hil: HIL):
+	hil_out = hil.dout("Millan", "HIL_OUT")
+	hil_in = hil.ain("Millan", "HIL_IN")
+
+	for _i in range(3):
+		for state in range(2): # 0, 1
+			print("\nHIL_OUT: 0")
+			hil_out.state = state
+			time.sleep(1)
+			hil_in_state = hil_in.state
+
+			target = state * 5.0
+			same = abs(hil_in_state - target) < 0.1
+			print(f"HIL_IN: {target} == {hil_in_state}: {same}")
+
+			time.sleep(2)
 
 		print()
 # ---------------------------------------------------------------------------- #
@@ -40,6 +54,7 @@ if __name__ == "__main__":
 	hil.load_config("config_millan.json")
 	hil.load_pin_map("millan_net_map.csv", "stm32f407_pin_map.csv")
 	
-	test_do_di(hil)
+	# test_do_di(hil)
+	test_do_ai(hil)
 
 	hil.shutdown()
