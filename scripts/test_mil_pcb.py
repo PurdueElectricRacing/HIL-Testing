53604cdd570e05e643b5dabca2233b94d00f14cd
Millan Kumar <millankumar@gmail.com>
2024-11-16 20:23:32 -0500

    Updating test to match net_map and config changes

--- scripts/test_mil_pcb.py
+++ scripts/test_mil_pcb.py
@@ -5,7 +5,7 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from hil.hil import HIL
 import hil.utils as utils
 import time
-# import random
+import random
 
 
 # ---------------------------------------------------------------------------- #
@@ -17,16 +17,16 @@ def bool_to_color_str(same: bool) -> str:
 
 
 def test_do_di(hil: HIL):
-	hil_do = hil.dout("Millan", "HIL_DO") # A1
-	hil_di = hil.din("Millan", "HIL_DI")  # A2
+	hil_a1 = hil.dout("Millan", "HIL_A1") # DO -> HIL writes
+	hil_a2 = hil.din ("Millan", "HIL_A2") # DI -> HIL reads
 
 	for _i in range(3):
 		for state in [0, 1]:
-			hil_do.state = state
+			hil_a1.state = state
 
 			time.sleep(0.2)
 
-			hil_di_state = hil_di.state
+			hil_di_state = hil_a2.state
 			same = hil_di_state == state
 
 			print(f"{hil_di_state} == {state} -> {bool_to_color_str(same)}")
@@ -38,18 +38,17 @@ def test_do_di(hil: HIL):
 		print()
 
 def test_dac_ai(hil: HIL):
-	hil_ao = hil.aout("Millan", "HIL_AO") # DAC1
-	hil_ai = hil.ain("Millan", "HIL_AI")  # A4
+	hil_dac1 = hil.aout("Millan", "HIL_DAC1") # AO -> HIL writes
+	hil_a4   = hil.ain ("Millan", "HIL_A3")   # AI -> HIL reads
 
 	for _i in range(3):
-		# random_voltage = random.uniform(0.0, 5.0)
-		for voltage in [0.34]:
-			# 5 * 255 / (2^12 - 1) = 0.31135531135
-			# 5 * 0.1 / 0.31135531135 = 1.60588235297
-			hil_ao.state = voltage
+		random_voltage = random.uniform(0.0, 5.0)
+		voltages = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, random_voltage]
+		for voltage in voltages:
+			hil_dac1.state = voltage
 			time.sleep(0.2)
 
-			hil_ai_voltage = hil_ai.state
+			hil_ai_voltage = hil_a4.state
 			within = abs(hil_ai_voltage - voltage) < 0.1
 
 			print(f"{hil_ai_voltage:1.2f} == {voltage:1.2f} -> {bool_to_color_str(within)}")
@@ -59,9 +58,9 @@ def test_dac_ai(hil: HIL):
 		print()
 
 def test_rly(hil: HIL):
-	hil_do = hil.dout("Millan", "HIL_DO")   # A1
-	hil_rly = hil.dout("Millan", "HIL_RLY") # RLY1
-	hil_di  = hil.din("Millan", "HIL_DI")   # A2
+	hil_a1   = hil.dout("Millan", "HIL_A1")   # DO -> HIL writes
+	hil_rly1 = hil.dout("Millan", "HIL_RLY1") # DO -> HIL writes
+	hil_a2   = hil.din ("Millan", "HIL_A2")   # DI -> HIL reads
 
 	# A1 -> RLY1+
 	# RLY1- -> A2
@@ -69,14 +68,14 @@ def test_rly(hil: HIL):
 	for _i in range(3):
 		for do_state in [0, 1]:
 			for rly_state in [0, 1]:
-				hil_do.state = do_state
-				hil_rly.state = rly_state
+				hil_a1.state = do_state
+				hil_rly1.state = rly_state
 
 				expected_state = do_state and rly_state
 
 				time.sleep(0.2)
 
-				hil_di_state = hil_di.state
+				hil_di_state = hil_a2.state
 				same = hil_di_state == expected_state
 
 				print(f"{hil_di_state} == {expected_state} -> {bool_to_color_str(same)}")
