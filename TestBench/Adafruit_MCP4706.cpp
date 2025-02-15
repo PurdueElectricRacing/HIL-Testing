cd83048b4faec4538bde8ebe44d9dd845b9033f6
Millan Kumar <millankumar@gmail.com>
2025-02-15 11:07:30 -0500

    Using my slightly modified driver
    https://github.com/LelsersLasers/MCP4706_Driver

--- TestBench/Adafruit_MCP4706.cpp
+++ TestBench/Adafruit_MCP4706.cpp
@@ -1,26 +1,12 @@
 /**************************************************************************/
 /*! 
-	@file     Adafruit_MCP4706.cpp
-	@author   K.Townsend (Adafruit Industries)
-	@license  BSD (see license.txt)
-	
-	Modified for MCP4706 by Pio Baettig
-
-	I2C Driver for Microchip's MCP4706 I2C DAC
-
-	This is a library for the MCP4706 8-bit DAC modified from
-    Adafruit MCP4725 library
-	----> https://www.adafruit.com/products/???
-		
-	Adafruit invests time and resources providing this open source code, 
-	please support Adafruit and open-source hardware by purchasing 
-	products from Adafruit!
-
-	@section  HISTORY
-
-	v1.0 - First release
+    Authors:
+    - Original: K.Townsend (Adafruit Industries)
+    - Modified for MCP4706 by Pio Baettig
+    - Now modifed by Millan kumar for PER HIL 2.0 2025 usage
 */
 /**************************************************************************/
+
 #if ARDUINO >= 100
  #include "Arduino.h"
 #else
@@ -33,38 +19,41 @@
 
 /**************************************************************************/
 /*! 
-	@brief  Instantiates a new MCP4706 class
+    @brief  Instantiates a new MCP4706 class
 */
 /**************************************************************************/
 Adafruit_MCP4706::Adafruit_MCP4706() { }
 
 /**************************************************************************/
 /*! 
-	@brief  Setups the HW
+    @brief  Setups the HW
 */
 /**************************************************************************/
-void Adafruit_MCP4706::begin(uint8_t addr) {
-	_i2caddr = addr;
-	Wire.begin();
+void Adafruit_MCP4706::begin(uint8_t addr, TwoWire &wire) {
+    _i2caddr = addr;
+    _wire = &wire;
+
+    // Also, user needs to setSCL and setSDA before calling begin
+    // Wire.begin(); 
 }
  
 /**************************************************************************/
 /*! 
-	@brief  Sets the output voltage to a fraction of source vref. (Value
-		can be 0..255)
+    @brief  Sets the output voltage to a fraction of source vref. (Value
+        can be 0..255)
 
-	@param[in]  output
-		The 8-bit value representing the relationship between
-		the DAC's input voltage and its output voltage.
+    @param[in]  output
+        The 8-bit value representing the relationship between
+        the DAC's input voltage and its output voltage.
 */
 /**************************************************************************/
-void Adafruit_MCP4706::setVoltage( uint8_t output) {
-	uint8_t twbrback = TWBR;
-	TWBR = 12; // 400 khz
-	// TWBR = 72; // 100 khz
-	Wire.beginTransmission(_i2caddr);
-	Wire.write(0);      // First Byte 0
-	Wire.write(output); // Second byte: Data bits          (D7.D6.D5.D4.D3.D2.D1.D0)
-	Wire.endTransmission();
-	TWBR = twbrback;
+void Adafruit_MCP4706::setVoltage(uint8_t output) {
+    uint8_t twbrback = TWBR;
+    TWBR = 12; // 400 khz
+    // TWBR = 72; // 100 khz
+    _wire->beginTransmission(_i2caddr);
+    _wire->write(0);      // First Byte 0
+    _wire->write(output); // Second byte: Data bits (D7.D6.D5.D4.D3.D2.D1.D0)
+    _wire->endTransmission();
+    TWBR = twbrback;
 }
--- TestBench/Adafruit_MCP4706.h
+++ TestBench/Adafruit_MCP4706.h
@@ -1,41 +1,27 @@
 /**************************************************************************/
 /*! 
-    @file     Adafruit_MCP4706.h
-    @author   K.Townsend (Adafruit Industries)
-	@license  BSD (see license.txt)
-	
-        Modified for MCP4706 by Pio Baettig
-
-	I2C Driver for Microchip's MCP4706 I2C DAC
-
-	This is a library for the MCP4706 8-bit DAC modified from
-        Adafruit MCP4725 library
-	----> https://www.adafruit.com/products/???
-		
-	Adafruit invests time and resources providing this open source code, 
-	please support Adafruit and open-source hardware by purchasing 
-	products from Adafruit!
-
-	@section  HISTORY
-
-    v1.0 - First release
+  Authors:
+  - Original: K.Townsend (Adafruit Industries)
+  - Modified for MCP4706 by Pio Baettig
+  - Now modifed by Millan kumar for PER HIL 2.0 2025 usage
 */
 /**************************************************************************/
 
 #if ARDUINO >= 100
- #include "Arduino.h"
+    #include "Arduino.h"
 #else
- #include "WProgram.h"
+    #include "WProgram.h"
 #endif
 
 #include <Wire.h>
 
 class Adafruit_MCP4706{
- public:
-  Adafruit_MCP4706();
-  void begin(uint8_t a);  
-  void setVoltage( uint8_t output);
+    public:
+        Adafruit_MCP4706();
+        void Adafruit_MCP4706::begin(uint8_t addr, TwoWire &wire = Wire); 
+        void setVoltage(uint8_t output);
 
- private:
-  uint8_t _i2caddr;
+    private:
+        uint8_t _i2caddr;
+        TwoWire *_wire;
 };
