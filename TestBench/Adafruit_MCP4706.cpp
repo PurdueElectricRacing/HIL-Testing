88dea70c738c1963ff6d7e0d6ed6c1302d1ca6ca
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-05-25 14:05:54 -0600

    Power down modes + defines
    Gave up on submodules, just copying the files here

--- TestBench/Adafruit_MCP4706.cpp
+++ TestBench/Adafruit_MCP4706.cpp
@@ -50,8 +50,28 @@ void Adafruit_MCP4706::setVoltage(uint8_t output) {
     TWBR = 12; // 400 khz
     // TWBR = 72; // 100 khz
     _wire->beginTransmission(_i2caddr);
-    _wire->write(0);      // First Byte 0
-    _wire->write(output); // Second byte: Data bits (D7.D6.D5.D4.D3.D2.D1.D0)
+    _wire->write(MCP4706_CMD_VOLDAC); // First byte: Command (CMD_VOLDAC = 0)
+    _wire->write(output);             // Second byte: Data bits (D7.D6.D5.D4.D3.D2.D1.D0)
     _wire->endTransmission();
     TWBR = twbrback;
+}
+
+
+/**************************************************************************/
+/*! 
+    @brief  Puts the DAC into a low power state using the specified resistor mode
+
+    @param[in]  mode
+        Power-down mode, one of:
+        - MCP4706_AWAKE
+        - MCP4706_PWRDN_1K
+        - MCP4706_PWRDN_100K
+        - MCP4706_PWRDN_500K
+*/
+/**************************************************************************/
+void Adafruit_MCP4706::setMode(uint8_t mode) {
+    uint8_t config = (mode & ~MCP4706_PWRDN_MASK); // ensure only power-down bits are set
+    _wire->beginTransmission(_i2caddr);
+    _wire->write(config | MCP4706_CMD_VOLCONFIG);  // command with config register
+    _wire->endTransmission();
 }
